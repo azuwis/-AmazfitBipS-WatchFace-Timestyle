bca28645919bacf918664f7ec0292768c73640e8
Zhong Jianxin <azuwis@gmail.com>
2020-06-09 14:10:34 +0800

    Larger font for weekday

--- gen-weekday.sh
+++ gen-weekday.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-index=21
-size=34x12
+index=41
+size=36x12
 
 for i in MON TUE WED THU FRI SAT SUN
 do
   file="$(printf "%04d" "$index").png"
-  convert -background none -fill white -trim -font LECO-1976-Regular.otf +antialias -pointsize 16 label:"$i" "$file"
+  convert -background none -fill white -trim -font LECO-1976-Regular.otf +antialias -pointsize 17 label:"$i" "$file"
   mogrify -background none -extent "$size" -gravity center "$file"
   index="$((index+1))"
 done
