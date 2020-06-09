85f859c786be6e2a37b01982be59750dd4f2b262
Zhong Jianxin <azuwis@gmail.com>
2020-06-09 14:25:01 +0800

    Larger font for month

--- gen-month.sh
+++ gen-month.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-index=56
+index=21
 size=34x12
-extend=200x12
+extend=210x12
 
 month=1
 
@@ -19,11 +19,11 @@ do
     background=black
     gravity=west
   else
-    file="${month}.png"
+    file="tmp-${month}.png"
     background=black
     gravity=west
   fi
-  convert -background "$background" -fill white -trim -font LECO-1976-Regular.otf +antialias -pointsize 16 label:"$label" "$file"
+  convert -background "$background" -fill white -trim -font LECO-1976-Regular.otf +antialias -pointsize 17 label:"$label" "$file"
   mogrify -background "$background" -extent "$size" -gravity center "$file"
   mogrify -background none -extent "$extend" -gravity "$gravity" "$file"
   if [ "$month" -ge 11 ]
@@ -33,3 +33,4 @@ do
   fi
   month="$((month+1))"
 done
+rm tmp-11.png tmp-12.png
