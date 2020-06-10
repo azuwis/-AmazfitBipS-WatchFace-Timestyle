63294ae3d65e58d9cd8ceefa7a43765f6b209b29
Zhong Jianxin <azuwis@gmail.com>
2020-06-10 17:52:31 +0800

    Larger font for steps

--- gen-digital.sh
+++ gen-digital.sh
@@ -6,7 +6,7 @@ index=11
 for i in 0 1 2 3 4 5 6 7 8 9
 do
   file="$(printf "%04d" "$index").png"
-  convert -background none -fill white -trim -font LECO-1976-Regular.otf +antialias -pointsize 15 label:"$i" "$file"
+  convert -background none -fill white -trim -font LECO-1976-Regular.otf +antialias -pointsize 16 label:"$i" "$file"
   # mogrify -background none -extent "$size" -gravity center "$file"
   index="$((index+1))"
 done
